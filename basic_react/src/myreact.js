8dd9a30d4945346570c8577bf52f5172e783c6ba
Soham Kulkarni <sohamkulkarni.insnapsys@gmail.com>
2025-10-14 15:04:28 +0530

    Printing Hello world and react on webpage.

---
+++ basic_react/src/myreact.js
@@ -0,0 +1,7 @@
+function Reac(){
+    return(
+        <h1>React in Javascript...</h1>
+    )
+}
+
+export default Reac;
---
+++ vite_react/src/myreact.jsx
@@ -0,0 +1,6 @@
+function Reac(){
+    return(
+        <h1>React in Javascript in Vite...</h1>
+    )
+}
+export default Reac;
